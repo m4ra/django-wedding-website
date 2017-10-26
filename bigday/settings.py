531a28a94258d1be4f75544458beffb509bc2b02
Mara Karagianni <mara@multiplace.org>
2017-10-26 22:44:23 +0300

    fix static root

--- bigday/settings.py
+++ bigday/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = 'static_root'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root/')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'bigday', 'static'),
