f61a9664c077b8bfba8a30127aed25031e1f315c
Mara Karagianni <mara@multiplace.org>
2017-10-26 22:14:17 +0300

    fix static root

--- bigday/settings.py
+++ bigday/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 STATIC_ROOT = 'static_root'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join('bigday', 'static'),
+    os.path.join(BASE_DIR, 'bigday', 'static'),
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
